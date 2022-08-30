9a31ea0d4d1b91c856284040bd8d017678539fb5
veeranki2014 <veeranki2014@gmail.com>
2022-08-30 13:12:05 +0530

    RoboShop Project Implementation.

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -9,7 +9,7 @@ resource "aws_spot_instance_request" "app_cheap_worker" {
 
   tags                    = {
     Name                  = "${element(var.APP_COMPONENTS,count.index )}-${var.ENV}"
-    Monitor               = yes
+    Monitor               = "yes"
   }
 }
 
@@ -57,11 +57,21 @@ resource "aws_route53_record" "db_records" {
   filename = "/tmp/inv-roboshop"
 }*/
 
-resource "local_file" "inventory-file" {
+##Approach one##############
+/*resource "local_file" "inventory-file" {
   content  = "[FRONTEND]\n${aws_spot_instance_request.app_cheap_worker.*.private_ip[5]}\n[PAYMENT]\n${aws_spot_instance_request.app_cheap_worker.*.
   private_ip[4]}\n[SHIPPING]\n${aws_spot_instance_request.app_cheap_worker.*.private_ip[3]}\n[USER]\n${aws_spot_instance_request.app_cheap_worker.*.
   private_ip[2]}\n[CATALOGUE]\n${aws_spot_instance_request.app_cheap_worker.*.private_ip[1]}\n[CART]\n${aws_spot_instance_request.app_cheap_worker.*.
   private_ip[0]}\n[REDIS]\n${aws_spot_instance_request.db_cheap_worker.*.private_ip[3]}\n[RABBITMQ]\n${aws_spot_instance_request.db_cheap_worker.*.
   private_ip[2]}\n[MONGODB]\n${aws_spot_instance_request.db_cheap_worker.*.private_ip[1]}\n[MYSQL]\n${aws_spot_instance_request.db_cheap_worker.*.private_ip[0]}"
   filename = "/tmp/inv-roboshop"
+}*/
+
+locals {
+  COMPONENTS = concat(aws_spot_instance_request.db_cheap_worker.*.private_ip, aws_spot_instance_request.app_cheap_worker.*.private_ip)
+}
+
+resource "local_file" "inventory-file" {
+  content     = "[FRONTEND]\n${local.COMPONENTS[9]}\n[PAYMENT]\n${local.COMPONENTS[8]}\n[SHIPPING]\n${local.COMPONENTS[7]}\n[USER]\n${local.COMPONENTS[6]}\n[CATALOGUE]\n${local.COMPONENTS[5]}\n[CART]\n${local.COMPONENTS[4]}\n[REDIS]\n${local.COMPONENTS[3]}\n[RABBITMQ]\n${local.COMPONENTS[2]}\n[MONGODB]\n${local.COMPONENTS[1]}\n[MYSQL]\n${local.COMPONENTS[0]}\n"
+  filename    = "/tmp/inv-roboshop-${var.ENV}"
 }
